cff3958ec218a5bf77ecdc9a9e650cd6708d41bf
Anthony Ashton <aac@Anthonys-MBP.lan>
2024-08-31 13:15:41 +0100

    Add argocd bootstrap to minikube provision

--- local-cluster/scripts/minikube-deprovision.py
+++ local-cluster/scripts/minikube-deprovision.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import subprocess
 
 def stop_minikube_instance(environment):
--- local-cluster/scripts/minikube-provision.py
+++ local-cluster/scripts/minikube-provision.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import subprocess
 
 def create_minikube_instance(environment):
@@ -11,10 +13,23 @@ def create_minikube_instance(environment):
     except subprocess.CalledProcessError as e:
         print(f"Failed to create Minikube instance for {environment} environment: {e}")
 
+def bootstrap_argocd(environment):
+    """
+    Bootstraps ArgoCD onto the Minikube cluster for the given environment.
+    """
+    cmd = f"kubectl --context k8s-{environment} apply -k ../argocd-bootstrap/overlays/{environment}"
+
+    try:
+        subprocess.run(cmd, shell=True, check=True)
+        print(f"Successfully bootstrapped ArgoCD for {environment} environment")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to bootstrap ArgoCD for {environment} environment: {e}")
+
 def main():
     environments = ['dev', 'int', 'prod']
     for env in environments:
         create_minikube_instance(env)
+        bootstrap_argocd(env)
 
 if __name__ == "__main__":
     main()
